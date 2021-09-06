8c4fc589672557cd0619d88c11add4ec001a595e
ArwaIndonesia <arwa.lanetteam.com>
2021-09-06 12:23:53 +0530

    router modified

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/user.js");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
+    console.log("TOKEN", token, process.env.JWT_SECRET);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decoded._id,
